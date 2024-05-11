57f90d5b0d3179c62425a58170e77ba2e5ca2abe
UgVo <32958860+UgVo@users.noreply.github.com>
2024-05-11 18:26:43 +0200

    EventWidget class added, still in WIP
    Signed-off-by: UgVo <32958860+UgVo@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,32 @@
 #include <QApplication>
 
 #include "apiconnector.h"
+#include "eventwidget.h"
 #include "mainwindow.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QFontDatabase::addApplicationFont(QStringLiteral(":/font/NotoColorEmoji.ttf"));
 
     MainWindow w;
+
+    QFont serifFont("Sans Serif", 12);
+
+    QSharedPointer<Event> event = QSharedPointer<Event>::create(
+        "🥳 test", "", QDateTime(QDate(2024, 10, 26), QTime(11, 3), QTimeZone(3600)),
+        QDateTime(QDate(2024, 10, 26), QTime(11, 6), QTimeZone(3600)));
+    EventWidget testEventWidget(event);
+    testEventWidget.setFont(serifFont);
+
+    QSharedPointer<Event> event2 = QSharedPointer<Event>::create(
+        "Test Event", "FREQ=MONTHLY;BYDAY=WE",
+        QDateTime(QDate(2024, 10, 26), QTime(11, 3), QTimeZone(3600)),
+        QDateTime(QDate(2024, 10, 26), QTime(11, 6), QTimeZone(3600)));
+    EventWidget testEventWidgetReccu(event2);
+    testEventWidgetReccu.setFont(serifFont);
+
     w.show();
+    testEventWidget.show();
+    testEventWidgetReccu.show();
     return a.exec();
 }
